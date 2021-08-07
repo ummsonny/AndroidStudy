bcd848bb854ef88e8c130e0fb0083b27c5438f64
ummsonny <zhtkdzjtkd@naver.com>
2021-08-07 20:07:06 +0900

    delete

--- Mission/mission14/app/src/main/java/org/techtown/mission14/OnThingItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package org.techtown.mission14;
-
-import android.view.View;
-
-public interface OnThingItemClickListener {
-    public void onClick(ThingAdapter.ViewHolder holder, View view, int position);
-}
